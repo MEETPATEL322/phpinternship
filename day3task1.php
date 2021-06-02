ddc38a4dc2bdf3759aa42336a3697e5c156dcf3c
VAGDODA MEETKUMAR KIRTIBHAI <84794036+MEETPATEL322@users.noreply.github.com>
2021-06-02 08:17:28 +0530

    Add files via upload

---
+++ day3task1.php
@@ -0,0 +1,60 @@
+<html>
+    <body>
+        <form action="day3task1dataprocess.php" method="post">
+            <table border="2" bgcolor="pink">
+               
+                 <tr>
+                    <td>
+                    NAME: <input type="text" name="txt1"/>   
+                    </td>
+                </tr>
+                 <tr>
+                    <td>
+                        MOBILENO: <input type="number" name="txt2"/>
+                    </td>
+                </tr>
+                 <tr>
+                    <td>
+                        AGE:<input type="number" name="txt3"/>
+                    </td>
+                </tr>
+                                 <tr>
+                    <td>
+                        ADRESS:<input type="text" name="txt4"/>
+                    </td>
+                </tr>
+                 <tr>
+                    <td>
+                        CITY:<input type="text" name="txt5"/>
+                     
+                    </td>
+                </tr>
+                 <tr>
+                    <td>
+                       PINCODE: <input type="number" name="txt6"/>
+                    </td>
+                </tr>
+                 <tr>
+                    <td>
+                        STATE:<input type="text" name="txt7"/>
+                    </td>
+                </tr>
+                 <tr>
+                    <td>
+                        EMAIL:<input type="text" name="txt8"/>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <input type="submit"/>
+                        <input type="reset"/>
+                        
+                    </td>
+                    </tr>
+            </table>
+            
+            
+        </form>
+    </body>
+</html>
+
