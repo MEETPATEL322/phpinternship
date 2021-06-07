6b1a5ba2071f0fc1521e73ecd2ac65184d99857f
VAGDODA MEETKUMAR KIRTIBHAI <84794036+MEETPATEL322@users.noreply.github.com>
2021-06-07 12:03:44 +0530

    day6half

---
+++ New folder/index.php
@@ -0,0 +1,151 @@
+<?php 
+	$host = "localhost";
+	$username = "root";
+	$passwd = "";
+	$dbname = "db_internship";
+
+	$connection = mysqli_connect($host, $username, $passwd, $dbname);
+        if($_POST){
+            $name = $_POST['name'];
+            $fname = $_POST['father_name'];
+            $address = $_POST['address'];
+            $gender = $_POST['gender'];
+            $state = $_POST['state'];
+            $city = $_POST['city'];
+            $dob = $_POST['birth_date'];
+            $pincode = $_POST['pincode'];
+            $course = $_POST['course'];
+            $email = $_POST['email'];
+            $q = mysqli_query($connection,"insert into tbl_student2(st_name,st_fname,st_address,st_gender,st_state,st_city,st_dob,st_pincode,st_course,st_email) values('{$name}','{$fname}','{$address}','{$gender}','{$state}','{$city}','{$dob}','{$pincode}','{$course}','{$email}')")
+            or die("Error ". mysqli_error($connection));
+            if ($q){
+                echo "<script>alert('Record Added');</script>";
+                
+            }
+            
+            }
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sign Up Form by Aniket</title>
+
+    <!-- Font Icon -->
+    <link rel="stylesheet" href="fonts/material-icon/css/material-design-iconic-font.min.css">
+
+    <!-- Main css -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+    <div class="main">
+        <div class="container">
+            <div class="signup-content">
+                <div class="signup-img">
+                    <img src="images/signup-img.jpg" alt="">
+                </div>
+                <div class="signup-form">
+                    <form method="POST" class="register-form" id="register-form">
+                        <h2>student registration form</h2>
+                        <div class="form-row">
+                            <div class="form-group">
+                                <label for="name">Name :</label>
+                                <input type="text" name="name" id="name" required/>
+                            </div>
+                            <div class="form-group">
+                                <label for="father_name">Father Name :</label>
+                                <input type="text" name="father_name" id="father_name" required/>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="address">Address :</label>
+                            <input type="text" name="address" id="address" required/>
+                        </div>
+                        <div class="form-radio">
+                            <label for="gender" class="radio-label">Gender :</label>
+                            <div class="form-radio-item">
+                                <input type="radio" name="gender" id="male" checked>
+                                <label for="male">Male</label>
+                                <span class="check"></span>
+                            </div>
+                            <div class="form-radio-item">
+                                <input type="radio" name="gender" id="female">
+                                <label for="female">Female</label>
+                                <span class="check"></span>
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="form-group">
+                                <label for="state">State :</label>
+                                <div class="form-select">
+                                    <select name="state" id="state">
+                                        <option value="gj">Gujarat</option>
+                                        <option value="rj">Rajasthan</option>
+                                        <option value="dl">Delhi</option>
+                                        <option value="mh">Maharashtra</option>
+                                        <option value="mp">Madhya Pradesh</option>
+                                        <option value="ut">Uttrakhand</option>
+                                    </select>
+                                    <span class="select-icon"><i class="zmdi zmdi-chevron-down"></i></span>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="city">City :</label>
+                                <div class="form-select">
+                                    <select name="city" id="city">
+                                        <option value=""></option>
+                                        <option value="ah">Ahmedabad</option>
+                                        <option value="vd">Vadodra</option>
+                                        <option value="mh">Mehsana</option>
+                                        <option value="vi">Visnagar</option>
+                                        <option value="gn">Gandhinagar</option>
+                                        <option value="su">Surat</option>
+                                    </select>
+                                    <span class="select-icon"><i class="zmdi zmdi-chevron-down"></i></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="birth_date">DOB :</label>
+                            <input type="text" name="birth_date" id="birth_date">
+                        </div>
+                        <div class="form-group">
+                            <label for="pincode">Pincode :</label>
+                            <input type="text" name="pincode" id="pincode">
+                        </div>
+                        <div class="form-group">
+                            <label for="course">Course :</label>
+                            <div class="form-select">
+                                <select name="course" id="course">
+                                    <option value=""></option>
+                                    <option value="computer">Computer Operator & Pragramming Assistant</option>
+                                    <option value="desiger">Designer</option>
+                                    <option value="marketing">Marketing</option>
+                                </select>
+                                <span class="select-icon"><i class="zmdi zmdi-chevron-down"></i></span>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="email">Email ID :</label>
+                            <input type="email" name="email" id="email" />
+                        </div>
+                        <div class="form-submit">
+                            <input type="submit" value="Reset All" class="submit" name="reset" id="reset" />
+                            <input type="submit" value="Submit Form" class="submit" name="submit" id="submit" />
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <!-- JS -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="js/main.js"></script>
+</body><!-- This templates was made by Colorlib (https://colorlib.com) -->
+</html>
